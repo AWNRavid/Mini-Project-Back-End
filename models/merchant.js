de6c63eff3e7d2c1084a8770bfcc717a9a124949
unknown <afwan.rizky96@gmail.com>
2022-04-09 19:09:22 +0700

    add register query connection inside register function in models merchant

--- models/merchant.js
+++ models/merchant.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-require('dotenv').config()
+require('dotenv').config();
 const { v4: uuidv4 } = require('uuid');
 
 let connection = mysql.createConnection({
@@ -8,5 +8,28 @@ let connection = mysql.createConnection({
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   port: process.env.PORT_DB,
-  
 });
+
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log('connected');
+  }
+});
+
+class ModelMerchant {
+  static Register(data) {
+    const query = `INSERT INTO merchant (id, password, name, address, phone_number) VALUES (?,?,?,?,?)`;
+    const values = [uuidv4(), data.password, data.name, data.address, data.phone_number];
+
+    connection.query(query, values, (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(result);
+      }
+    });
+    // return data;
+  }
+}
