7035e52d1f9fc4bd162165ae385447befeaf5413
unknown <afwan.rizky96@gmail.com>
2022-04-09 19:11:03 +0700

    add get-all-merchant function in routes merchant

--- routes/merchant.js
+++ routes/merchant.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const ControllerMerchant = require('../controllers/merchant');
 
 router.post('/register', ControllerMerchant.Register);
+router.get('/get-all-merchant', ControllerMerchant.GetAllMerchant);
 
 module.exports = router;
