0c1abdc34379826df321ce180fea9fcb8b4fe918
unknown <afwan.rizky96@gmail.com>
2022-04-09 19:16:30 +0700

    add delete function in routes merchant

--- routes/merchant.js
+++ routes/merchant.js
@@ -5,6 +5,7 @@ const ControllerMerchant = require('../controllers/merchant');
 router.post('/register', ControllerMerchant.Register);
 router.get('/get-all-merchant', ControllerMerchant.GetAllMerchant);
 router.post('/login', ControllerMerchant.Login)
+router.delete('/delete-account/:id', ControllerMerchant.DeleteAccount);
 
 
 module.exports = router;
