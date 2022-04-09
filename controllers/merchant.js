1b1940b8b68242fb9775aba266b2b53e61fc3f77
unknown <afwan.rizky96@gmail.com>
2022-04-09 19:15:11 +0700

    add Login function in controllers mrechant

--- controllers/merchant.js
+++ controllers/merchant.js
@@ -13,4 +13,26 @@ class ControllerMerchant {
       res.status(200).json(result);
     });
   }
+
+  static Login(req, res) {
+    ModelMerchant.Login(req.body).then((result) => {
+      // console.log(result);
+      if (result.length == 0) {
+        console.log(`user not found`);
+        res.status(404).json('not found');
+      } else {
+        if (req.body.password == result[0].password) {
+          // console.log(`Login success`);
+          res.status(200).json(`login success`);
+          isLogin = true;
+          console.log(isLogin);
+          id = result[0].id;
+          // this.DeleteAccount(req, res, true, result[0].id);
+        } else {
+          // console.log(`Incorrect password`);
+          res.status(400).json(`Incorrect password`);
+        }
+      }
+    });
+  }
 }
