c59a66d8f885dac64ce37d8120277e7d2b168b30
unknown <afwan.rizky96@gmail.com>
2022-04-09 19:14:30 +0700

    add login function in routes merchant

--- routes/merchant.js
+++ routes/merchant.js
@@ -4,5 +4,7 @@ const ControllerMerchant = require('../controllers/merchant');
 
 router.post('/register', ControllerMerchant.Register);
 router.get('/get-all-merchant', ControllerMerchant.GetAllMerchant);
+router.post('/login', ControllerMerchant.Login)
+
 
 module.exports = router;
